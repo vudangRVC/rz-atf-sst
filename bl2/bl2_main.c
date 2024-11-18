e830d3bfac1e61549c30d8e39ed21ea1958d2907
Quoc Trinh <quoc.trinh.fz@renesas.com>
2024-11-18 17:07:42 +0700

    renesas: g2l: sbc: add option to load and boot CM33 from ATF (#2)
    Change includes:
    	- Support REMOVE_UBOOT option to enable loading and booting CM33 from ATF.
    	By using that option, U-Boot can be removed from the system boot,
    	and all images for CM33 and CA55 can be loaded and booted from BL2.
    
    Signed-off-by: Vu Dang <vu.dang.te@renesas.com>

--- bl2/bl2_main.c
+++ bl2/bl2_main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2025, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2013-2023, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -11,18 +11,20 @@
 #include <bl1/bl1.h>
 #include <bl2/bl2.h>
 #include <common/bl_common.h>
-#include <common/build_message.h>
 #include <common/debug.h>
 #include <drivers/auth/auth_mod.h>
 #include <drivers/auth/crypto_mod.h>
 #include <drivers/console.h>
 #include <drivers/fwu/fwu.h>
-#include <lib/bootmarker_capture.h>
 #include <lib/extensions/pauth.h>
-#include <lib/pmf/pmf.h>
 #include <plat/common/platform.h>
 
 #include "bl2_private.h"
+
+#ifdef REMOVE_UBOOT
+#include <lib/mmio.h>
+#include <drivers/delay_timer.h>
+#endif /* REMOVE_UBOOT */
 
 #ifdef __aarch64__
 #define NEXT_IMAGE	"BL31"
@@ -30,50 +32,198 @@
 #define NEXT_IMAGE	"BL32"
 #endif
 
-#if ENABLE_RUNTIME_INSTRUMENTATION
-	PMF_REGISTER_SERVICE(bl_svc, PMF_RT_INSTR_SVC_ID,
-		BL_TOTAL_IDS, PMF_DUMP_ENABLE);
-#endif
-
-/*******************************************************************************
- * The only thing to do in BL2 is to load further images and pass control to
- * next BL. The memory occupied by BL2 will be reclaimed by BL3x stages.
- ******************************************************************************/
-void __no_pauth bl2_main(u_register_t arg0, u_register_t arg1, u_register_t arg2,
-	       u_register_t arg3)
-{
-	entry_point_info_t *next_bl_ep_info;
-
-	/* Enable early console if EARLY_CONSOLE flag is enabled */
-	plat_setup_early_console();
 #if RESET_TO_BL2
-
+/*******************************************************************************
+ * Setup function for BL2 when RESET_TO_BL2=1
+ ******************************************************************************/
+void bl2_el3_setup(u_register_t arg0, u_register_t arg1, u_register_t arg2,
+		   u_register_t arg3)
+{
 	/* Perform early platform-specific setup */
 	bl2_el3_early_platform_setup(arg0, arg1, arg2, arg3);
 
 	/* Perform late platform-specific setup */
 	bl2_el3_plat_arch_setup();
+
+#if CTX_INCLUDE_PAUTH_REGS
+	/*
+	 * Assert that the ARMv8.3-PAuth registers are present or an access
+	 * fault will be triggered when they are being saved or restored.
+	 */
+	assert(is_armv8_3_pauth_present());
+#endif /* CTX_INCLUDE_PAUTH_REGS */
+}
 #else /* RESET_TO_BL2 */
+
+/*******************************************************************************
+ * Setup function for BL2 when RESET_TO_BL2=0
+ ******************************************************************************/
+void bl2_setup(u_register_t arg0, u_register_t arg1, u_register_t arg2,
+	       u_register_t arg3)
+{
 	/* Perform early platform-specific setup */
 	bl2_early_platform_setup2(arg0, arg1, arg2, arg3);
 
 	/* Perform late platform-specific setup */
 	bl2_plat_arch_setup();
+
+#if CTX_INCLUDE_PAUTH_REGS
+	/*
+	 * Assert that the ARMv8.3-PAuth registers are present or an access
+	 * fault will be triggered when they are being saved or restored.
+	 */
+	assert(is_armv8_3_pauth_present());
+#endif /* CTX_INCLUDE_PAUTH_REGS */
+}
 #endif /* RESET_TO_BL2 */
 
-	if (is_feat_pauth_supported()) {
-#if BL2_RUNS_AT_EL3
-		pauth_init_enable_el3();
-#else
-		pauth_init_enable_el1();
-#endif
-	}
+#ifdef REMOVE_UBOOT
+/*******************************************************************************
+ * DEFINE CM33
+ ******************************************************************************/
+/*
+ * Clock pulse generater (CPG) registers for CM33
+ */
+#define CPG_SIPLL3_MON                  (0x1101013C)    // PLL3 (SSCG) Monitor Register
+#define CPG_CLKON_CM33                  (0x11010504)    // Clock Control Register Cortex-M33
+#define CPG_CLKMON_CM33                 (0x11010684)    // Clock Monitor Register Cortex-M33
+#define CPG_RST_CM33                    (0x11010804)    // Reset Control Register Cortex-M33
+#define CPG_RSTMON_CM33                 (0x11010984)    // Reset Monitor Register Cortex-M33
 
-#if ENABLE_RUNTIME_INSTRUMENTATION
-	PMF_CAPTURE_TIMESTAMP(bl_svc, BL2_ENTRY, PMF_CACHE_MAINT);
-#endif
+/*
+ * System Controller (SYSC) registers for CM33
+ */
+#define SYS_CM33_CFG0                   (0x11020804)    // CM33 Config Register0
+#define SYS_CM33_CFG1                   (0x11020808)    // CM33 Config Register1
+#define SYS_CM33_CFG2                   (0x1102080C)    // CM33 Config Register2
+#define SYS_CM33_CFG3                   (0x11020810)    // CM33 Config Register3
+#define SYS_CM33_CTL                    (0x11020818)    // CM33 Control Register
+#define SYS_LSI_MODE                    (0x11020A00)    // LSI Mode Signal Register
+#define SYS_LP_CM33CTL1                 (0x11020D28)    // Lowpower Sequence CM33 Control Register1
 
-	NOTICE("BL2: %s\n", build_version_string);
+/*
+ * The following values are required to start up the Cortex-M33.
+ * For more details, refer to RZ/G2L Group, RZ/G2LC Group User's Manual: Hardware
+ * See section 3. System CPU Cortex-M33, chapter 3.4.1 Startup Sequence
+ */
+#define SYSTICK_TIMER_ON		0x00103CE5
+#define SYSTICK_TIMER_OFF		0x00003D08
+#define CLK_ENABLED_IN_NORMAL_MODE	0x00010001
+#define CLK_ENABLED_IN_DEBUG_MODE	0x00030003
+#define RST_SIGN_STOP			0x00070007
+
+#define CM33_START_NORMAL	"start_normal" // Option to start in normal mode
+#define CM33_START_DEBUG	"start_debug"  // Option to start in debug mode
+#define NORMAL_MODE		0
+#define DEBUG_MODE		1
+#define SECURE_VECTOR_ADDR	0x1001FF80 // Secure vector address of CM33
+#define NON_SECURE_VECTOR_ADDR	0x00010000 // Non secure vector address of CM33
+
+static void cm33_boot_normal_mode(void);
+static void cm33_boot_debug_mode(void);
+
+void write_register(uintptr_t addr, uint32_t value)
+{
+        mmio_write_32(addr, value);
+        if(mmio_read_32(addr) != value){
+                INFO("BL2: Write register addr = 0x%lx <-- value = 0x%x - failed\n", addr, value);
+        }
+        else {
+                INFO("BL2: Write register addr = 0x%lx <-- value = 0x%x - passed\n", addr, value);
+        }
+}
+
+void cm33_boot_normal_mode(void)
+{
+        NOTICE("CM33: Start in normal boot mode\n");
+
+        // Supply clock to CM33_CLKIN
+        write_register(CPG_CLKON_CM33, CLK_ENABLED_IN_NORMAL_MODE);
+        NOTICE("CM33: Supplied clock\n");
+
+        // Poll CPG_CLKMON_CM33 to confirm that CM33_CLKIN clock is supplied
+        while (mmio_read_32(CPG_CLKMON_CM33) != 0x1)
+                mdelay(10);
+
+        // Stop the reset signals (released from the reset state)
+        write_register(CPG_RST_CM33, RST_SIGN_STOP);
+        NOTICE("CM33: Stopped the reset signals\n");
+
+        // Poll CPG_RSTMON_CM33 to confirm that all the reset signals are not applied
+        while(mmio_read_32(CPG_RSTMON_CM33) != 0)
+                mdelay(10);
+}
+
+void cm33_boot_debug_mode(void)
+{
+        NOTICE("CM33: Start in debug boot mode\n");
+
+	// Supply clock to CM33_TSCLK and CM33_CLKIN
+        write_register(CPG_CLKON_CM33, CLK_ENABLED_IN_DEBUG_MODE);
+        NOTICE("CM33: Supplied clock\n");
+
+        // Poll CPG_CLKMON_CM33 to confirm that both CM33_TSCLK and CM33_CLKIN clock are supplied
+        while (mmio_read_32(CPG_CLKMON_CM33) != 0x3)
+                mdelay(10);
+
+        // Set DEBUGQREQn bit of SYS_LP_CM33CTL1 to 1
+        // Fixme. Lacking of SYS_LP_CM33CTL1 info
+
+        // Poll SYS_LP_CM33CTL1 to check if DEBUGQACCEPTn bit becomes 1
+        // Fixme. Lacking of SYS_LP_CM33CTL1.DEBUGQACCEPTn info
+
+        // Set FETCHCNT bit of SYS_CM33_CTL register to 1
+        // Fixme. Lacking of SYS_CM33_CTL info
+
+        // Stop the reset signals (released from the reset state)
+        write_register(CPG_RST_CM33, RST_SIGN_STOP);
+        NOTICE("CM33: Stopped the reset signals\n");
+
+        // Poll CPG_RSTMON_CM33 to confirm that all the reset signals are not applied
+        while (mmio_read_32(CPG_RSTMON_CM33) != 0)
+                mdelay(10);
+
+        // Set FETCHCNT bit of SYS_CM33_CTL register to 0
+        // Fixme. Lacking of SYS_CM33_CTL info
+}
+
+void cm33_start(unsigned char debug, uint32_t s_addr, uint32_t ns_addr)
+{
+        // Check if the SSCG PLL3 is ON or not
+        if ((CPG_SIPLL3_MON & 0x1) == 0x1) {
+                write_register(SYS_CM33_CFG0, SYSTICK_TIMER_ON);
+                write_register(SYS_CM33_CFG1, SYSTICK_TIMER_ON);
+        } else {
+                write_register(SYS_CM33_CFG0, SYSTICK_TIMER_OFF);
+                write_register(SYS_CM33_CFG1, SYSTICK_TIMER_OFF);
+        }
+
+        // Set the secure vector address of Cortex-M33
+        write_register(SYS_CM33_CFG2 , s_addr);
+
+        // Set the non secure vector address of Cortex-M33
+        write_register(SYS_CM33_CFG3 , ns_addr);
+
+        // Start the CM33 propram in normal/debug mode
+        debug ? cm33_boot_debug_mode() : cm33_boot_normal_mode();
+}
+
+void kick_cm33() {
+        // Start CM33 program in normal mode by default
+        cm33_start(NORMAL_MODE, SECURE_VECTOR_ADDR, NON_SECURE_VECTOR_ADDR);
+}
+#endif /* REMOVE_UBOOT */
+
+/*******************************************************************************
+ * The only thing to do in BL2 is to load further images and pass control to
+ * next BL. The memory occupied by BL2 will be reclaimed by BL3x stages. BL2
+ * runs entirely in S-EL1.
+ ******************************************************************************/
+void bl2_main(void)
+{
+	entry_point_info_t *next_bl_ep_info;
+
+	NOTICE("BL2: %s\n", version_string);
 	NOTICE("BL2: %s\n", build_message);
 
 	/* Perform remaining generic architectural setup in S-EL1 */
@@ -97,10 +247,13 @@ void __no_pauth bl2_main(u_register_t arg0, u_register_t arg1, u_register_t arg2
 	/* Load the subsequent bootloader images. */
 	next_bl_ep_info = bl2_load_images();
 
+#ifdef REMOVE_UBOOT
+	/* Kick CM33 */
+	kick_cm33();
+#endif /* REMOVE_UBOOT */
+
 	/* Teardown the Measured Boot backend */
 	bl2_plat_mboot_finish();
-
-	crypto_mod_finish();
 
 #if !BL2_RUNS_AT_EL3
 #ifndef __aarch64__
@@ -112,18 +265,14 @@ void __no_pauth bl2_main(u_register_t arg0, u_register_t arg1, u_register_t arg2
 	disable_mmu_icache_secure();
 #endif /* !__aarch64__ */
 
+	console_flush();
+
+#if ENABLE_PAUTH
 	/*
 	 * Disable pointer authentication before running next boot image
 	 */
-	if (is_feat_pauth_supported()) {
-		pauth_disable_el1();
-	}
-
-#if ENABLE_RUNTIME_INSTRUMENTATION
-	PMF_CAPTURE_TIMESTAMP(bl_svc, BL2_EXIT, PMF_CACHE_MAINT);
-#endif
-
-	console_flush();
+	pauth_disable_el1();
+#endif /* ENABLE_PAUTH */
 
 	/*
 	 * Run next BL image via an SMC to BL1. Information on how to pass
@@ -135,17 +284,14 @@ void __no_pauth bl2_main(u_register_t arg0, u_register_t arg1, u_register_t arg2
 
 	NOTICE("BL2: Booting " NEXT_IMAGE "\n");
 	print_entry_point_info(next_bl_ep_info);
-#if ENABLE_RUNTIME_INSTRUMENTATION
-	PMF_CAPTURE_TIMESTAMP(bl_svc, BL2_EXIT, PMF_CACHE_MAINT);
-#endif
 	console_flush();
 
+#if ENABLE_PAUTH
 	/*
 	 * Disable pointer authentication before running next boot image
 	 */
-	if (is_feat_pauth_supported()) {
-		pauth_disable_el3();
-	}
+	pauth_disable_el3();
+#endif /* ENABLE_PAUTH */
 
 	bl2_run_next_image(next_bl_ep_info);
 #endif /* BL2_RUNS_AT_EL3 */
--- plat/renesas/rz/common/bl2_plat_mem_params_desc.c
+++ plat/renesas/rz/common/bl2_plat_mem_params_desc.c
@@ -7,6 +7,7 @@
 #include <common/desc_image_load.h>
 #include <plat/common/platform.h>
 #include <rzg2l_def.h>
+#include <platform_def.h>
 
 #if (RZG2L_BL33_EXECUTION_EL == 0)
 #define BL33_MODE MODE_EL1
@@ -30,13 +31,17 @@ static bl_mem_params_node_t bl2_mem_params_descs[] = {
 		.image_info.image_max_size = BL31_LIMIT - BL31_BASE,
 		.image_info.image_base = BL31_BASE,
 
-# ifdef BL32_BASE
+#ifdef BL32_BASE
 		.next_handoff_image_id = BL32_IMAGE_ID,
-# else
+#else
+#ifdef REMOVE_UBOOT
+		.next_handoff_image_id = FW_CONFIG_ID,
+#else
 		.next_handoff_image_id = BL33_IMAGE_ID,
-# endif /* BL32_BASE */
+#endif /* REMOVE_UBOOT */
+#endif /* BL32_BASE */
 	},
-# ifdef BL32_BASE
+#ifdef BL32_BASE
 	{
 		.image_id = BL32_IMAGE_ID,
 
@@ -52,21 +57,114 @@ static bl_mem_params_node_t bl2_mem_params_descs[] = {
 
 		.next_handoff_image_id = BL33_IMAGE_ID,
 	},
-# endif /* BL32_BASE */
+#endif /* BL32_BASE */
+#ifdef REMOVE_UBOOT
 	{
+		.image_id = FW_CONFIG_ID,
+
+		SET_STATIC_PARAM_HEAD(ep_info, PARAM_EP, VERSION_2,
+				entry_point_info_t, NON_SECURE | NON_EXECUTABLE),
+		.ep_info.pc = FW_CONFIG_BASE,
+		.ep_info.spsr = 0,
+
+		SET_STATIC_PARAM_HEAD(image_info, PARAM_EP, VERSION_2,
+				image_info_t, 0),
+		.image_info.image_max_size = FW_CONFIG_LIMIT - FW_CONFIG_BASE,
+		.image_info.image_base = FW_CONFIG_BASE,
+
+		.next_handoff_image_id = HW_CONFIG_ID,
+	}, /* Finish load for rzv2l_cm33_rpmsg_demo_secure_code.bin */
+	{
+		.image_id = HW_CONFIG_ID,
+
+		SET_STATIC_PARAM_HEAD(ep_info, PARAM_EP, VERSION_2,
+				entry_point_info_t, NON_SECURE | NON_EXECUTABLE),
+		.ep_info.pc = HW_CONFIG_BASE,
+		.ep_info.spsr = 0,
+
+		SET_STATIC_PARAM_HEAD(image_info, PARAM_EP, VERSION_2,
+				image_info_t, 0),
+		.image_info.image_max_size = HW_CONFIG_LIMIT - HW_CONFIG_BASE,
+		.image_info.image_base = HW_CONFIG_BASE,
+
+		.next_handoff_image_id = SOC_FW_CONFIG_ID,
+	}, /* Finish load for rzv2l_cm33_rpmsg_demo_non_secure_vector.bin */
+	{
+		.image_id = SOC_FW_CONFIG_ID,
+
+		SET_STATIC_PARAM_HEAD(ep_info, PARAM_EP, VERSION_2,
+				entry_point_info_t, NON_SECURE | NON_EXECUTABLE),
+		.ep_info.pc = SOC_FW_CONFIG_BASE,
+		.ep_info.spsr = 0,
+
+		SET_STATIC_PARAM_HEAD(image_info, PARAM_EP, VERSION_2,
+				image_info_t, 0),
+		.image_info.image_max_size = SOC_FW_CONFIG_LIMIT - SOC_FW_CONFIG_BASE,
+		.image_info.image_base = SOC_FW_CONFIG_BASE,
+
+		.next_handoff_image_id = RMM_IMAGE_ID,
+	}, /* Finish load for rzv2l_cm33_rpmsg_demo_secure_vector.bin */
+	{
+		.image_id = RMM_IMAGE_ID,
+
+		SET_STATIC_PARAM_HEAD(ep_info, PARAM_EP, VERSION_2,
+				entry_point_info_t, NON_SECURE | EXECUTABLE),
+		.ep_info.pc = RMM_FW_BASE,
+		.ep_info.spsr = 0,
+
+		SET_STATIC_PARAM_HEAD(image_info, PARAM_EP, VERSION_2,
+				image_info_t, 0),
+		.image_info.image_max_size = RMM_FW_LIMIT - RMM_FW_BASE,
+		.image_info.image_base = RMM_FW_BASE,
+
+		.next_handoff_image_id = BL331_IMAGE_ID,
+	}, /* Finish load for rzv2l_cm33_rpmsg_demo_non_secure_code.bin */
+#endif /* REMOVE_UBOOT */
+	{
+#ifdef REMOVE_UBOOT
+                .image_id = BL331_IMAGE_ID,
+
+		SET_STATIC_PARAM_HEAD(ep_info, PARAM_EP, VERSION_2,
+				entry_point_info_t, NON_SECURE | NON_EXECUTABLE),
+		.ep_info.pc = BL331_BASE,
+		.ep_info.spsr = 0,
+
+		SET_STATIC_PARAM_HEAD(image_info, PARAM_EP, VERSION_2,
+				image_info_t, 0),
+		.image_info.image_max_size = BL331_LIMIT - BL331_BASE,
+		.image_info.image_base = BL331_BASE,
+
+		.next_handoff_image_id = BL332_IMAGE_ID,
+	},
+	{
+		.image_id = BL332_IMAGE_ID,
+#else
 		.image_id = BL33_IMAGE_ID,
+#endif /* REMOVE_UBOOT */
 
 		SET_STATIC_PARAM_HEAD(ep_info, PARAM_EP, VERSION_2,
 			entry_point_info_t, NON_SECURE | EXECUTABLE),
 		.ep_info.spsr = SPSR_64(BL33_MODE, MODE_SP_ELX,
 			DISABLE_ALL_EXCEPTIONS),
+#ifdef REMOVE_UBOOT
+		.ep_info.pc = BL332_BASE,
+#ifdef RZ_BL332_ARG0
+		.ep_info.args.arg0 = RZ_BL332_ARG0,
+#endif /* RZ_BL332_ARG0 */
+#else
 		.ep_info.pc = BL33_BASE,
+#endif /* REMOVE_UBOOT */
 
 		SET_STATIC_PARAM_HEAD(image_info, PARAM_EP, VERSION_2,
 			image_info_t, 0),
 		.image_info.image_max_size =
+#ifdef REMOVE_UBOOT
+				(uint32_t) (BL332_LIMIT - BL332_BASE),
+		.image_info.image_base = BL332_BASE,
+#else
 				(uint32_t) (BL33_LIMIT - BL33_BASE),
 		.image_info.image_base = BL33_BASE,
+#endif /* REMOVE_UBOOT */
 
 		.next_handoff_image_id = INVALID_IMAGE_ID,
 	}
