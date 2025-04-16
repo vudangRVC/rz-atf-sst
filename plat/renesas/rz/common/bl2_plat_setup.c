642e1d21ebdf85253b7b42b760da28f8a80a6cbf
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2025-04-16 15:59:13 +0700

    rzv2l: Atf pass 4 value register to u-boot v2l
    Changes include:
    	- Write board id to register x2 and pass to u-boot.
    	- Write value register x0, x1, x3 and pass to u-boot.
    
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- plat/renesas/rz/common/bl2_plat_setup.c
+++ plat/renesas/rz/common/bl2_plat_setup.c
@@ -70,7 +70,10 @@ int bl2_plat_handle_post_image_load(unsigned int image_id)
 		break;
 	case BL33_IMAGE_ID:
 		/* Write board ID to x2 register to pass through u-boot */
-		bl_mem_params->ep_info.args.arg2 = BOARD_ID_RZPI;
+		bl_mem_params->ep_info.args.arg0 = RZ_BL332_ARG0;
+		bl_mem_params->ep_info.args.arg1 = 0x01;
+		bl_mem_params->ep_info.args.arg2 = BOARD_ID_RZV2L;
+		bl_mem_params->ep_info.args.arg3 = 0x03;
 		memcpy(&params->bl33_ep_info, &bl_mem_params->ep_info,
 			sizeof(entry_point_info_t));
 		break;
