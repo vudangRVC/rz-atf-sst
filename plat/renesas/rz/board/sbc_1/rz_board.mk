52dbb9138d7670a50ae4d99bdc25a884616a4ece
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2025-05-12 12:13:44 +0700

    renesas: rzpi: add rzpi dts
    Changes include:
    	- add rzpi dts.
    
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- plat/renesas/rz/board/sbc_1/rz_board.mk
+++ plat/renesas/rz/board/sbc_1/rz_board.mk
@@ -10,3 +10,5 @@ DDR_SOURCES +=  plat/renesas/rz/soc/${PLAT}/drivers/ddr/param_mc_issi8.c \
 DDR_PLL4    := 1600
 $(eval $(call add_define,DDR_PLL4))
 
+# Default Device tree
+DTB_FILE_NAME		?=	rzpi
