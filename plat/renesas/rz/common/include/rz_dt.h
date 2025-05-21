15e819be9ea256110fcc7260a8938286344901f9
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2025-05-21 18:13:43 +0700

    renesas: rzv2h: Add function read_prop_from_subnode
    Changes include:
    	- Add function read_prop_from_subnode.
    
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- plat/renesas/rz/common/include/rz_dt.h
+++ plat/renesas/rz/common/include/rz_dt.h
@@ -13,5 +13,7 @@
  * Function and variable prototypes
  ******************************************************************************/
 int dt_validation(uintptr_t dt_addr);
+int8_t read_prop_from_subnode(void *fdt, const char *node, const char *sub_node, const char *prop_name,
+	uint8_t index, uint32_t *value);
 
 #endif /* RZ_DT_H */
--- plat/renesas/rz/common/rz_dt.c
+++ plat/renesas/rz/common/rz_dt.c
@@ -26,3 +26,27 @@ int dt_validation(uintptr_t dt_addr)
 
 	return ret;
 }
+
+int8_t read_prop_from_subnode(void *fdt, const char *node, const char *sub_node, const char *prop_name,
+	uint8_t index, uint32_t *value)
+{
+	// Get node offset
+	int node_offset = fdt_path_offset(fdt, node);
+	NOTICE("node_offset = %d\n", node_offset);
+
+	// Get sub node offset
+	int sub_node_offset = fdt_subnode_offset(fdt, node_offset, sub_node);
+	NOTICE("sub_node_offset = %d\n", sub_node_offset);
+
+	uint32_t target = 0;
+	int32_t len = 0;
+	const fdt32_t *val = fdt_getprop(fdt, sub_node_offset, prop_name, &len);
+	if (!val || len < 4) {
+		NOTICE("Missing or invalid property: %s\n", prop_name);
+		return -1;
+	}
+	target = fdt32_to_cpu(val[index]);
+	NOTICE("target = 0x%x\n", target);
+	*value = target;
+	return 0;
+}
