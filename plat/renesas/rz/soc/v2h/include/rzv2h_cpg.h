aa5c453dfccab9539cc56709ba6009cf6fbd37e2
Son Nguyen <son.nguyen.xg@bp.renesas.com>
2025-06-09 10:13:13 +0700

    renesas: rzv2h: Add support device tree on RZ/V2H.
    Changes include:
    	- Add support for ATF boot using the FCONF framework
                  and device tree on RZ/V2H.
    
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- plat/renesas/rz/soc/v2h/include/rzv2h_cpg.h
+++ plat/renesas/rz/soc/v2h/include/rzv2h_cpg.h
@@ -4,11 +4,11 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#ifndef __CPG_H__
-#define __CPG_H__
+#ifndef __RZV2H_CPG_H__
+#define __RZV2H_CPG_H__
 
 void cpg_early_setup(void);
-void cpg_setup(void);
+void rzv2h_cpg_setup(void *fdt);
 void cpg_ddr0_part1(void);
 void cpg_ddr0_part2(void);
 void cpg_ddr1_part1(void);
@@ -16,4 +16,4 @@ void cpg_ddr1_part2(void);
 void cpg_prepare_suspend(void);
 void cpg_ddr_pwrokin_off(uint8_t base);
 
-#endif /* __CPG_H__ */
+#endif /* __RZV2H_CPG_H__ */
