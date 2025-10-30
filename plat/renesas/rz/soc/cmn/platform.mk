f252c640f56be3c365929c66d6b16dbd901e4c3f
Quoc Trinh <quoc.trinh.fz@renesas.com>
2025-10-30 16:59:23 +0700

    rz-cmn: Add suport Geniatech board rs-g2l100
    Changes include:
    	- Add yaml file, ddr params and dts file.
    
    Signed-off-by: Son Nguyen <son.nguyen.xg@bp.renesas.com>

--- plat/renesas/rz/soc/cmn/platform.mk
+++ plat/renesas/rz/soc/cmn/platform.mk
@@ -21,7 +21,7 @@ DDR_SOURCES				+=	plat/renesas/rz/soc/${PLAT}/drivers/ddr/lpddr.c				\
 							plat/renesas/rz/soc/${PLAT}/drivers/ddr/ddr_misc.c			\
 							plat/renesas/rz/soc/${PLAT}/drivers/ddr/plat_ddr_setup.c
 
-DTB_LIST := rzg2l-sbc.dtb r9a07g044l2-smarc.dtb r9a07g054l2-smarc.dtb rzv2h-evk-ver1.dtb rzv2h-sbc-ver1.dtb rzv2h-rdk-ver1.dtb
+DTB_LIST := rzg2l-sbc.dtb r9a07g044l2-smarc.dtb r9a07g054l2-smarc.dtb rzv2h-evk-ver1.dtb rzv2h-sbc-ver1.dtb rzv2h-rdk-ver1.dtb rs-g2l100.dtb
 DTB_OUT := $(BUILD_PLAT)/fdts
 
 dtbs: $(DTB_LIST:%=$(DTB_OUT)/%)
